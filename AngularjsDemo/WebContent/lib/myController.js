bfc114ffd4fc867163dd1f521051f3fb3979bf99
manuc <manuc@192.168.0.25>
2020-07-12 19:43:54 -0400

    First commit

---
+++ AngularjsDemo/WebContent/lib/myController.js
@@ -0,0 +1,23 @@
+app.controller("ctrl", function($scope){
+	$scope.un="";
+	$scope.pw="";
+	$scope.msg="";
+	$scope.msg1="";
+	$scope.fun=function(e){
+		if($scope.un.length==0){
+			$scope.msg="enter your name";
+			e.preventDefault();
+		}
+		else{
+			$scope.msg="";
+		}
+		if($scope.pw.length==0){
+			$scope.msg1="enter your password"
+				e.preventDefault();	
+		}
+		else{
+			$scope.msg1="";
+		}
+			
+	}
+})
